02c0af7cf6aee5fbb377e7fea01da6e68eade694
Tommy P. Keane <talk@tommypkeane.com>
2020-08-28 21:42:08 -0400

    ENH: Creating Nginx Container
    - Using the `nginx:latest` Docker image from Docker Hub
    - Creating a simple container that binds to port 80 internally but is
    accesible through port 8080 externally (outside the container, _i.e._ on
    the host system).

--- pulumi_basic_nginx/__main__.py
+++ pulumi_basic_nginx/__main__.py
@@ -1,3 +1,26 @@
 """A Python Pulumi program"""
 
-import pulumi
+import pulumi;
+import pulumi_docker as docker;
+
+nginx_image = docker.RemoteImage(
+   "nginx",
+   name= "nginx",
+);
+
+ngninx_container = docker.Container(
+   "nginx",
+   image= nginx_image.latest,
+   hosts= [
+      {
+         "host": "nginx",
+         "ip": "0.0.0.0",
+      },
+   ],
+   ports= [
+      {
+         "internal": 80,
+         "external": 8080,
+      },
+   ],
+);
